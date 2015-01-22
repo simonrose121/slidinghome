a3f15bf16668fa1dc2eaca621ef9e4a3bf1ec149
Simon Rose <simonrose121@gmail.com>
2015-01-22 11:09:54 +0000

    added game grid object

---
+++ source/gameGrid.cpp
@@ -0,0 +1,4 @@
+#include "gameGrid.h"
+
+
+
---
+++ source/gameGrid.h
@@ -0,0 +1,78 @@
+#if !defined(__GAMEGRID_H__)
+#define __GAMEGRID_H__
+
+template<typename T, int width, int height>
+class GameGrid {
+public:
+	T data[width * height];
+
+	stDiterator begin() {
+		return iterator(data);
+	}
+
+	iterator end() {
+		return iterator(data + width * height);
+	}
+
+	class iterator {
+	public:
+		iterator(const iterator &other) :
+			ptr(other.ptr)
+		{
+		}
+
+		iterator &left() const {
+			return iterator(ptr - 1);
+		}
+
+		iterator &right() const {
+			return iterator(ptr + 1);
+		}
+
+		iterator &up() const {
+			return iterator(ptr - width);
+		}
+
+		iterator &down() const {
+			return iterator(ptr + width);
+		}
+
+		iterator &operator++() {
+			++ptr;
+			return *this;
+		}
+
+		iterator &operator--() {
+			--ptr;
+			return *this;
+		}
+
+		iterator operator++(int) {
+			++*this;
+			return iterator(ptr + 1);
+		}
+
+		iterator operator--(int) {
+			--*this;
+			return iterator(ptr - 1);
+		}
+
+		T operator*() const {
+			return *ptr;
+		}
+
+	private:
+		iterator();
+		iterator(T *ptr_) :
+			ptr(ptr_)
+		{
+		}
+
+		T *ptr;
+
+		friend class Grid;
+	};
+};
+
+
+#endif
